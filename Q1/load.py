4ec5a43fa0c4f254b9241a25c5bd5d54f9ceba89
Alexsoil <alexsoilgr@gmail.com>
2022-07-02 18:35:41 +0300

    New stuff

--- Q1/load.py
+++ Q1/load.py
@@ -1,6 +1,9 @@
+from ctypes.wintypes import tagSIZE
 import pandas as pd
 import os
+import fnmatch
 
+#pd.set_option('display.max_rows', None)
 
 try:
     genome = pd.read_pickle("pickleJar" + os.path.sep + "genome_tags.pkl")
@@ -14,6 +17,23 @@ except:
     movie = pd.read_csv("archive" + os.path.sep + "movie.csv")
     rating = pd.read_csv("archive" + os.path.sep + "rating.csv")
     tag = pd.read_csv("archive" + os.path.sep + "tag.csv")
+    # Refactor movies
+    title = []
+    date = []
+    for idx, row in movie.iterrows():
+        temp = row['title'].rstrip().replace("\"", "")
+        try:
+            date.append(int(temp[-5:-1]))
+            title.append(temp[:-6])
+        except:
+            date.append(3000)
+            title.append(temp)
+    movie['date'] = date
+    movie['title'] = title
+    # Refactor ratings
+    timestamp = []
+    print(rating)
+    # Pickle results
     genome.to_pickle("pickleJar" + os.path.sep + "genome_tags.pkl")
     movie.to_pickle("pickleJar" + os.path.sep + "movie.pkl")
     rating.to_pickle("pickleJar" + os.path.sep + "rating.pkl")
@@ -21,12 +41,20 @@ except:
     print("Data loaded and saved to pickleJar")
 
 # movies by recent score
+# movieId title rating timestamp (movies rating)
 # movies by keyword sorted by score
+# movieId title rating (movies rating)
 # movies by genre sorted by score/release date
+# movieId title genre date rating
 # movies information by name (genre, score, top5 tags)
 # movies by tag sorted by score (top-n)
 
-print(genome)
-print(movie)
-print(rating)
-print(tag)
+Q1 = pd.DataFrame
+
+Q1 = movie[['movieId', 'title']].merge(rating[['movieId', 'rating', 'timestamp']], on='movieId', how='outer')
+Q1.to_pickle("pickleJar" + os.path.sep + "Q1_table.pkl")
+print(Q1)
+#print(movie[['movieId', 'title']])
+
+#Q1['movieId'] = movie['']
+#print(tag)
--- Q1/main.py
+++ Q1/main.py
@@ -1,6 +1,9 @@
 from importlib.metadata import metadata
 import connect_db
+import pandas as pd
+import os
 from cassandra import cluster
+
 
 try:
     con = connect_db.connect()
@@ -8,10 +11,16 @@ try:
     clstr = con[1]
     data = cluster.Metadata.keyspaces
     print(data)
-    print("Please, I beg you")
+    exit()
+    row = session.execute("DROP TABLE IF EXISTS movies_by_rating;")
+    row = session.execute("CREATE TABLE movies_by_rating (movieId int, title text, rating float, stamp timestamp, PRIMARY KEY ((movieId), stamp) ) WITH comment = 'Q1. Find best rated movies on specific timeframe';")
+    print(row)
+    Q1 = pd.read_pickle("pickleJar" + os.path.sep + "Q1_table.pkl")
+    for idx, row in Q1.iterrows():
+        query = "insert into movies_by_rating (movieId, title, rating, stamp) values (" + str(row['movieId']) + ", '" + str(row['title'].rstrip()) + "', " + str(row['rating']) + ", '" + str(row['timestamp']) + "');"
+        session.execute(query)
+        exit()
     connect_db.disconnect(clstr)
-    print("Yo I'm adding this guy to friends`")
-    
 except Exception as e:
     print(e)
     print("H porta kollhse")
