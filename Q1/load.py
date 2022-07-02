0c961533e55c3ac9054c64ff9782846a3f5c31b6
kyrlm <kyriaki.lm@gmail.com>
2022-07-02 19:54:57 +0300

    changes

--- Q1/load.py
+++ Q1/load.py
@@ -54,6 +54,39 @@ Q1 = pd.DataFrame
 Q1 = movie[['movieId', 'title']].merge(rating[['movieId', 'rating', 'timestamp']], on='movieId', how='outer')
 Q1.to_pickle("pickleJar" + os.path.sep + "Q1_table.pkl")
 print(Q1)
+
+Q2 = pd.DataFrame
+
+Q2 = movie[['movieId', 'title']].merge(rating[['movieId', 'rating']], on='movieId', how='outer')
+Q2.to_pickle("pickleJar" + os.path.sep + "Q2_table.pkl")
+print(Q2)
+
+Q3_1 = pd.DataFrame
+
+Q3_1 = movie[['movieId', 'title', 'genres']].merge(rating[['movieId', 'rating']], on='movieId', how='outer')
+Q3_1.to_pickle("pickleJar" + os.path.sep + "Q3_1_table.pkl")
+print(Q3_1)
+
+Q3_2 = pd.DataFrame
+
+Q3_2 = movie[['movieId', 'title', 'genres','date']]
+Q3_2.to_pickle("pickleJar" + os.path.sep + "Q3_2_table.pkl")
+print(Q3_2)
+
+#Q4 = pd.DataFrame
+
+#Q4 = movie[['movieId', 'title', 'genres']].merge(rating[['movieId', 'rating']], on='movieId', how='outer')
+#Q4 = Q4.merge(tag[['movieId', 'tag']], on='movieId', how='outer')
+#Q4.to_pickle("pickleJar" + os.path.sep + "Q4_table.pkl")
+#print(Q4)
+
+Q5 = pd.DataFrame
+
+Q5 = movie[['movieId', 'title']].merge(rating[['movieId', 'rating']], on='movieId', how='outer')
+Q5 = Q5.merge(tag[['movieId', 'tag']], on='movieId', how='outer')
+Q5.to_pickle("pickleJar" + os.path.sep + "Q5_table.pkl")
+print(Q5)
+
 #print(movie[['movieId', 'title']])
 
 #Q1['movieId'] = movie['']
