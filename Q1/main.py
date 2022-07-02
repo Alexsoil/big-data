ac636b7666eb13d32f7e72009e3b1eaf42b2653f
Alexsoil <alexsoilgr@gmail.com>
2022-07-02 19:27:54 +0300

    Maybe working db load

--- Q1/main.py
+++ Q1/main.py
@@ -11,7 +11,10 @@ try:
     clstr = con[1]
     data = cluster.Metadata.keyspaces
     print(data)
-    exit()
+    # rows = session.execute("select * from movies_by_rating;")
+    # for row in rows:
+    #     print ('MovieID: {id} Title:{title} Rating:{score} Timestamp:{date}'.format(id=row[0], title=row[3], score=row[2], date=row[1]))
+
     row = session.execute("DROP TABLE IF EXISTS movies_by_rating;")
     row = session.execute("CREATE TABLE movies_by_rating (movieId int, title text, rating float, stamp timestamp, PRIMARY KEY ((movieId), stamp) ) WITH comment = 'Q1. Find best rated movies on specific timeframe';")
     print(row)
@@ -19,8 +22,8 @@ try:
     for idx, row in Q1.iterrows():
         query = "insert into movies_by_rating (movieId, title, rating, stamp) values (" + str(row['movieId']) + ", '" + str(row['title'].rstrip()) + "', " + str(row['rating']) + ", '" + str(row['timestamp']) + "');"
         session.execute(query)
-        exit()
     connect_db.disconnect(clstr)
+    exit()  
 except Exception as e:
     print(e)
     print("H porta kollhse")
